324c459b1c64650b6a71c008f206fabd597e6bb7
Jeroen Engels <jfm.engels@gmail.com>
2019-09-18 00:26:22 +0200

    Change my profile picture

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -41,7 +41,7 @@ function Bio() {
 
 const bioQuery = graphql`
   query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+    avatar: file(absolutePath: { regex: "/profile-pic.png/" }) {
       childImageSharp {
         fixed(width: 50, height: 50) {
           ...GatsbyImageSharpFixed
