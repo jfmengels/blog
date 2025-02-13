03c16c92f0ac3bcafea8379b6efdebdb9eb786e6
Jeroen Engels <jfm.engels@gmail.com>
2025-02-13 10:00:19 +0100

    Fix favicon

--- app/Site.elm
+++ app/Site.elm
@@ -3,6 +3,8 @@ module Site exposing (config)
 import BackendTask exposing (BackendTask)
 import FatalError exposing (FatalError)
 import Head
+import MimeType
+import Pages.Url
 import Settings
 import SiteConfig exposing (SiteConfig)
 
@@ -18,5 +20,9 @@ head : BackendTask FatalError (List Head.Tag)
 head =
     [ Head.metaName "viewport" (Head.raw "width=device-width,initial-scale=1")
     , Head.sitemapLink "/sitemap.xml"
+    , Head.metaName "apple-mobile-web-app-capable" (Head.raw "yes")
+    , Head.metaName "apple-mobile-web-app-status-bar-style" (Head.raw "black-translucent")
+    , Head.icon [ ( 32, 32 ) ] MimeType.Png (Pages.Url.fromPath [ "favicon/favicon-32x32.png" ])
+    , Head.icon [ ( 16, 16 ) ] MimeType.Png (Pages.Url.fromPath [ "favicon/favicon-16x16.png" ])
     ]
         |> BackendTask.succeed
--- elm-pages.config.mjs
+++ elm-pages.config.mjs
@@ -7,9 +7,6 @@ export default {
   headTagsTemplate(context) {
     return `
 <link rel="stylesheet" href="/style.css" />
-<link rel="icon" href="/media/logo.svg">
-<link rel="mask-icon" href="/media/logo.svg" color="#000000">
-<link rel="apple-touch-icon" href="logo-180.png">
 <meta name="generator" content="elm-pages v${context.cliVersion}" />
 <meta name="theme-color" media="(prefers-color-scheme: light)" content="#ffffff" />
 <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#0f172a" />
